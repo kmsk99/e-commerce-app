709422a82503840eb0aa4957215c42cd8bc1145d
kmsk99 <momoqq@naver.com>
2021-10-26 11:00:38 +0900

    added user controller unit test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,23 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as faker from 'faker';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
+
+  const userId = faker.datatype.number();
+  const randomDate = faker.date.recent();
+  const randomUsername = faker.internet.userName();
+  const randomEmail = faker.internet.email();
+  const randomPassword = faker.internet.password();
+
+  const createUserDto: CreateUserDto = {
+    username: randomUsername,
+    email: randomEmail,
+    password: randomPassword,
+  };
+
+  const savedUser = {
+    id: userId,
+    createdAt: randomDate,
+    updatedAt: randomDate,
+    username: randomUsername,
+    email: randomEmail,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+    userController = module.get<UserController>(UserController);
   });
 
-  it.todo(
-    'should be defined',
-    // , () => {
-    //   expect(controller).toBeDefined();
-    // }
-  );
+  it('should be defined', () => {
+    expect(userController).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('/register', () => {
+    it('register succes', async () => {
+      const userServiceCreateSpy = jest
+        .spyOn(userService, 'create')
+        .mockResolvedValue(savedUser);
+
+      const result = await userController.create(createUserDto);
+
+      expect(userServiceCreateSpy).toHaveBeenCalledWith(createUserDto);
+      expect(userServiceCreateSpy).toBeCalledTimes(1);
+      expect(result).toBe(savedUser);
+    });
+  });
 });
